f8435656ba47218092e8faaefe16045711666cdb
Vinicius Pavin <vyper149@gmail.com>
2024-07-11 23:23:22 +0200

    Add admin nav items

--- src/components/global/navbar.tsx
+++ src/components/global/navbar.tsx
@@ -3,7 +3,7 @@
 import React from 'react'
 import Link from 'next/link'
 import { Cloud } from 'lucide-react'
-import { navbarItems } from '@/lib/constants'
+import { adminItems, navbarItems } from '@/lib/constants'
 import clsx from 'clsx'
 import { usePathname } from 'next/navigation'
 
@@ -12,7 +12,7 @@ export default function Navbar() {
     
     return (
         <aside className="hidden border-r bg-muted/40 md:block">
-            <div className="flex h-full max-h-screen flex-col gap-2">
+            <div className="flex h-full max-h-screen flex-col">
                 <div className="flex h-14 items-center px-4 lg:h-[60px] lg:px-6">
                     <Link href="/" className="flex items-center gap-2 font-semibold">
                         <Cloud className="h-6 w-6" />
@@ -20,8 +20,25 @@ export default function Navbar() {
                     </Link>
                 </div>
                 <div className="flex-1">
-                    <nav className="grid items-start px-2 text-sm font-medium lg:px-4">
+                    <nav className="grid items-start px-2 text-sm font-medium lg:px-4 py-4 lg:py-6">
+                        <span className="px-2 py-2 text-xs font-bold font-mono uppercase text-muted-foreground">Your infra</span>
                         {navbarItems.map(item => (
+                            <Link
+                            key={item.label}
+                            href={item.link}
+                            className={clsx(
+                                "flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary",
+                                {"bg-muted" : pathName === item.link}
+                            )}
+                        >
+                            {item.icon}
+                            {item.label}
+                            </Link>
+                        ))}
+                    </nav>
+                    <nav className="grid items-start px-2 text-sm font-medium lg:px-4">
+                        <span className="px-2 py-2 text-xs font-bold font-mono uppercase text-muted-foreground">Administration</span>
+                        {adminItems.map(item => (
                             <Link
                             key={item.label}
                             href={item.link}
--- src/lib/constants.tsx
+++ src/lib/constants.tsx
@@ -1,7 +1,11 @@
-import { Cylinder, Box, Server } from "lucide-react";
+import { Cylinder, Box, Server, Group } from "lucide-react";
 
 export const navbarItems = [
     {icon: <Server />,  label: "Nodes", link: "/nodes"},
     {icon: <Box />,  label: "Deployments", link: "/deployments"},
     {icon: <Cylinder />,  label: "Buckets", link: "/buckets"}
+]
+
+export const adminItems = [
+    {icon: <Group />,  label: "Namespaces", link: "/namespaces"},
 ]
